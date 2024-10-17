72b748106560fa473ee45f4900da35818667aad1
mckram <mck.ramsay@gmail.com>
2024-10-17 13:49:48 -0400

    test

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ hamburger.addEventListener('click', function () {
         menu.style.display = "block"
     }
 });
+
+  // When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("logo").style.height = "5rem";
+  } else {
+    document.getElementById("logo").style.height = "10rem";
+  }
+}
