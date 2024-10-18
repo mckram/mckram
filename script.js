cfe1d07a876d3979e7ff7f183e064aff2f0678ca
mckram <mck.ramsay@gmail.com>
2024-10-18 13:48:37 -0400

    test

--- script.js
+++ script.js
@@ -6,13 +6,22 @@ nav.addEventListener('click', function () {
     const crossIcon = this.querySelector('.cross-icon');
     if (hamIcon.style.display === "none") {
         hamIcon.style.display = "inline-block"
-        menu.style.display = "none"
+        menu.style.display = "inline-block"
         crossIcon.style.display = "none"
     }
     else {
         crossIcon.style.display = "inline-block"
         hamIcon.style.display = "none"
         menu.style.display = "inline-block"
+    }
+});
+
+menu.addEventListener('click', function () {
+    const hamIcon = this.querySelector('.hamburger-icon');
+    const crossIcon = this.querySelector('.cross-icon');
+    if (menu.style.display === "inline-block") {
+        menu.style.display = "none"
+        
     }
 });
 
