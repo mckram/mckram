4e107bc4ecf65d445a6dd69c98064851ae707ab9
mckram <mck.ramsay@gmail.com>
2024-10-20 13:25:17 -0400

    test

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ nav.addEventListener('click', function () {
     const crossIcon = this.querySelector('.cross-icon');
     if (hamIcon.style.display === "none") {
         hamIcon.style.display = "inline-block"
-        menu.style.display = "inline-block"
+        menu.style.display = "none"
         crossIcon.style.display = "none"
     }
     else {
@@ -16,29 +16,6 @@ nav.addEventListener('click', function () {
     }
 });
 
-menu.addEventListener('click', function () {
-    const hamIcon = this.querySelector('.hamburger-icon');
-    const crossIcon = this.querySelector('.cross-icon');
-    if (menu.style.display === "inline-block") {
-        menu.style.display = "none"     
-    }
-    else {
-        menu.style.display = "inline-block"
-    }
-});
-
-function myFunction(imgs) {
-    // Get the expanded image
-    var expandImg = document.getElementById("expandedImg");
-    // Get the image text
-    var imgText = document.getElementById("imgtext");
-    // Use the same src in the expanded image as the image being clicked on from the grid
-    expandImg.src = imgs.src;
-    // Use the value of the alt attribute of the clickable image as text inside the expanded image
-    imgText.innerHTML = imgs.alt;
-    // Show the container element (hidden with CSS)
-    expandImg.parentElement.style.display = "block";
-  };
 
   // When the user scrolls down 50px from the top of the document, resize the header's font size
 window.onscroll = function() {scrollFunction()};
